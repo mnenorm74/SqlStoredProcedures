d0bb0e134d8a017ca0191b487a319a58f5c1276d
Olga Bursina <bursina.olya@yandex.ru>
2020-11-15 14:37:08 +0500

    Menu added

--- SqlStoredProcedures/SqlConnector.cs
+++ SqlStoredProcedures/SqlConnector.cs
@@ -24,7 +24,38 @@ namespace SqlStoredProcedures
 
         private void ShowMenu()
         {
-            
+            Console.WriteLine("1. Выполнить свой запрос");
+            Console.WriteLine("2. Выбрать существующий запрос");
+            Console.WriteLine("3. Выбрать существующую хранимую процедуру");
+            Console.WriteLine("4. Выход");
+            var number = GetInputOrZero(Console.ReadLine());
+            ExecuteMenuPoint(number);
+        }
+
+        private int GetInputOrZero(string input)
+        {
+            var parsed = int.TryParse(input, out var number);
+            return parsed ? number : 0;
+        }
+        
+        private void ExecuteMenuPoint(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Введено некорректное значение!");
+                    ShowMenu();
+                    break;
+            }
         }
     }
 }
