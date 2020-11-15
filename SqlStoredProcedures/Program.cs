9bbf95ce995346af93da65593e3d9836b9253fb4
Olga Bursina <bursina.olya@yandex.ru>
2020-11-15 14:24:06 +0500

    SqlConnection added

---
+++ SqlStoredProcedures/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SqlStoredProcedures
+{
+    class Program
+    {
+        private const string serverName = "DESKTOP-P6TQKTV";
+        private const string dbName = "udb_Bursina";
+        private static readonly string connectionString = 
+            @$"Data Source={serverName};Initial Catalog={dbName};Integrated Security=True";
+        
+        static void Main(string[] args)
+        {
+            var connector = new SqlConnector();
+            connector.StartSqlInteraction(connectionString);
+        }
+    }
+}
---
+++ SqlStoredProcedures/SqlConnector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SqlStoredProcedures
+{
+    public class SqlConnector
+    {
+        public void StartSqlInteraction(string connectionString)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch
+                {
+                    Console.WriteLine("Не удалось установить подключение к БД");
+                    return;
+                }
+                ShowMenu();
+            }
+        }
+
+        private void ShowMenu()
+        {
+            
+        }
+    }
+}
