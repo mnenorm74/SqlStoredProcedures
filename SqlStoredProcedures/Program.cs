84dc63b8661de4bb0772ba6014950042323a37d5
Olga Bursina <bursina.olya@yandex.ru>
2020-11-15 17:42:42 +0500

    Db renamed

--- SqlStoredProcedures/Program.cs
+++ SqlStoredProcedures/Program.cs
@@ -3,7 +3,7 @@
     class Program
     {
         private const string serverName = "DESKTOP-P6TQKTV";
-        private const string dbName = "udb_Bursina";
+        private const string dbName = "Bursina_Konovalova";
         private static readonly string connectionString = 
             @$"Data Source={serverName};Initial Catalog={dbName};Integrated Security=True";
         
