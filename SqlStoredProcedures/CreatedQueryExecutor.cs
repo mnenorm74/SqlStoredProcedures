6b7f4cc5ec613c45ce9ea495ac208ed2dcb4eaa6
Olga Bursina <bursina.olya@yandex.ru>
2020-11-15 18:16:26 +0500

    Queries updated

--- SqlStoredProcedures/CreatedQueryExecutor.cs
+++ SqlStoredProcedures/CreatedQueryExecutor.cs
@@ -18,6 +18,9 @@ namespace SqlStoredProcedures
         {
             Console.Clear();
             Console.WriteLine("1. Вывести всех авторов из заданного города");
+            Console.WriteLine("2. Вывести имя и фамилия автора и название книги по id книги");
+            Console.WriteLine("3. Вывести количество строк заданной таблицы");
+            Console.WriteLine("4. Вывести полную выручку по всем книгам");
         }
         
         private static void ExecuteMenuPoint(int number)
@@ -25,9 +28,22 @@ namespace SqlStoredProcedures
             switch (number)
             {
                 case 1:
-                    Console.WriteLine("Введите город:");
+                    Console.WriteLine("Введите город, например, Москва");
                     var city = Console.ReadLine();
                     QueryExecutor.ExecuteQuery(SqlQueries.GetAuthorsFromCityQuery(city), sqlConnection);
+                    break;
+                case 2:
+                    Console.WriteLine("Введите id книги, например PS2106");
+                    var id = Console.ReadLine();
+                    QueryExecutor.ExecuteQuery(SqlQueries.GetNameAndTitleFromId(id), sqlConnection);
+                    break;
+                case 3:
+                    Console.WriteLine("Введите название таблицы, например, authors");
+                    var name = Console.ReadLine();
+                    QueryExecutor.ExecuteQuery(SqlQueries.GetLinesCount(name), sqlConnection);
+                    break;
+                case 4:
+                    QueryExecutor.ExecuteQuery(SqlQueries.GetProceeds(), sqlConnection);
                     break;
                 default:
                     Console.Clear();
--- SqlStoredProcedures/SqlQueries.cs
+++ SqlStoredProcedures/SqlQueries.cs
@@ -1,10 +1,34 @@
-﻿namespace SqlStoredProcedures
+﻿using System.Text;
+
+namespace SqlStoredProcedures
 {
     public static class SqlQueries
     {
+        //Все авторы по заданному городу
         public static string GetAuthorsFromCityQuery(string city)
         {
             return $"select * from authors where city = '{city}'";
+        }
+
+        //имя и фамилия автора и название книги по id книги (пример 'PS2106')
+        public static string GetNameAndTitleFromId(string id)
+        {
+            return new StringBuilder("select au_lname, au_fname, title from authors inner join titleauthor ")
+                .Append("inner join titles on titleauthor.title_id = titles.title_id ")
+                .Append($"on authors.au_id = titleauthor.au_id where titles.title_id= '{id}'")
+                .ToString();
+        }
+
+        //количество строк заданной таблицы
+        public static string GetLinesCount(string tableName)
+        {
+            return $"select count(*) from {tableName}";
+        }
+
+        //Полная выручка
+        public static string GetProceeds()
+        {
+            return "sum(ytd_sales*price) from titles";
         }
     }
 }
